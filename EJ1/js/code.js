79d0711e98a45c691c682f8b9edd5dd0d11d2c69
AlbertNoMes <albertnotariomestres@gmail.com>
2022-03-08 11:54:40 +0100

    commit ej1

---
+++ EJ1/js/code.js
@@ -0,0 +1,13 @@
+//ARCHIVO EXTERNO, SI
+//SEGUNDO MENSAJE, SI
+//AÑADIR COMENTARIOS, SI
+//AÑADIR AVISO (con alert), SI
+
+//PRIMER SCRIPT MOSTRADO EN CONSOLA (F12 Y CONSOLA o BOTÓN DERECHO, INSPECCIONAR Y CONSOLA (EN CHROME))
+console.log("Hola Mundo!");
+
+//SEGUNDO SCRIPT MOSTRADO EN CONSOLA
+console.log("Soy el primer script");
+
+//POPUP DE AVISO
+alert("¿Problemas visualizando la página? Comprueva que tu navegador tiene soporta para JavaScript");
